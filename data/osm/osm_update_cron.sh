c0b95ac2b515bc3a3a4a223d076b4dabbcdd56b5
Jeffrey Johnson <jjohnson@boundlessgeo.com>
2013-12-09 19:22:58 +0000

    Some small changes/fixes to osm_update_cron.sh

--- data/osm/osm_update_cron.sh
+++ data/osm/osm_update_cron.sh
@@ -7,9 +7,9 @@ time geogit add
 time geogit commit -m "Update PH OSM from geofabrik"
 time geogit osm map ../mapping.json
 rm -rf *
-time geogit shp export buildings_osm buildings_osm.shp
 time geogit shp export damage_lines damage_lines.shp
 time geogit shp export damage_polygons damage_polygons.shp
-time geogit shp export hospital_points hospital_points.shp
-time geogit shp export hospital_polygons hospital_polygons.shp
+time geogit shp export buildings_osm buildings_osm.shp
+time geogit shp export hospital_points_osm hospital_points_osm.shp
+time geogit shp export hospital_polygons_osm hospital_polygons_osm.shp
 time geonode importlayers -u boundless -o *.shp
